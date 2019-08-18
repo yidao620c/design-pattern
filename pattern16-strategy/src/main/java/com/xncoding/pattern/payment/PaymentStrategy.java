43abe1f4b9178db331cacb71e18cd17fe4713186
xiongneng <yidao620@gmail.com>
2019-08-18 16:19:32 +0800

    状态模式

---
+++ pattern16-strategy/src/main/java/com/xncoding/pattern/payment/PaymentStrategy.java
@@ -0,0 +1,14 @@
+package com.xncoding.pattern.payment;
+
+/**
+ * 支付工资的策略接口，公司有多种支付工资的算法
+ * 比如，现金、银行卡、现金加股票、现金加期权、美元支付等
+ */
+public interface PaymentStrategy {
+    /**
+     * 公司给某人真正支付工资
+     *
+     * @param ctx 支付工资的上下文，里面包含算法需要的数据
+     */
+    public void pay(PaymentContext ctx);
+}
---
+++ pattern16-strategy/src/main/java/com/xncoding/pattern/standard/ConcreteStrategyB.java
@@ -0,0 +1,10 @@
+package com.xncoding.pattern.standard;
+
+/**
+ * 实现具体的算法B
+ */
+public class ConcreteStrategyB implements Strategy {
+    public void algorithmInterface() {
+        //具体的算法实现
+    }
+}
