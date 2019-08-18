c84ecc13c14a606fd28c147aa8594df89ff954df
xiongneng <yidao620@gmail.com>
2019-08-18 21:59:11 +0800

    装饰器模式

---
+++ pattern21-decorator/src/main/java/com/xncoding/pattern/decorator/standard/Component.java
@@ -0,0 +1,11 @@
+package com.xncoding.pattern.decorator.standard;
+
+/**
+ * 组件对象的接口，可以给这些对象动态地添加职责
+ */
+public abstract class Component {
+    /**
+     * 示例方法
+     */
+    public abstract void operation();
+}
---
+++ pattern21-decorator/src/main/java/com/xncoding/pattern/decorator/standard/ConcreteComponent.java
@@ -0,0 +1,10 @@
+package com.xncoding.pattern.decorator.standard;
+
+/**
+ * 具体实现组件对象接口的对象
+ */
+public class ConcreteComponent extends Component {
+    public void operation() {
+        //相应的功能处理
+    }
+}
